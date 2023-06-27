3cd97d6ee654583885bfa3991c4e562e6bcbccfa
Sergey <hardmass3ffect@gmail.com>
2023-06-27 16:59:29 +0300

    add main block

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import s from './Main.module.css'
+import imgpreview from '../img/imgpreview.jpg'
 
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.text}>
-                <span>Hi There</span>
-                <h1>I'am Sergey Ostroukh</h1>
-                <p>Frontend Developer</p>
+            <div className={s.container}>
+                <div className={s.text}>
+                    <span>Hi There</span>
+                    <h1>I'am Sergey Ostroukh</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}>
+                    <img src={imgpreview} alt="img"/>
+                </div>
             </div>
-            <div className={s.photo}></div>
+
         </div>
     );
 };
